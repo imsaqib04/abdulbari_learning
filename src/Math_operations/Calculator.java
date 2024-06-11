21417d572bcf8535acd514e21e497426c7cc8a85
Mohd Saqib Ansari <you@example.com>
2024-06-11 23:28:58 +0530

    calci

---
+++ src/Math_operations/Calculator.java
@@ -0,0 +1,54 @@
+package Math_operations;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+        public static void main (String[] args){
+
+            float a ,b , result = 0;
+            int option;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the value of first");
+            a = sc.nextFloat();
+            System.out.println("enter the value of second");
+            b = sc.nextFloat();
+            System.out.println("choose 1 for add\nchoose 2 for sub\nchoose 3 for mul\nchoose 4 for div\n" );
+            System.out.println("choose option to perform");
+            option = sc.nextInt();
+
+            switch (option){
+
+                case 1:
+                    result = a+b;
+                    break;
+
+                case 2:
+                    result = a-b;
+                    break;
+
+                case 3:
+                    result = a*b;
+                    break;
+
+                case 4:
+                    if(a == 0){
+                        result = 0;
+                    }
+                    else if (b == 0){
+                        System.out.println("undifine");
+                    }
+                    else {
+                        result = a / b;
+                    }
+                    break;
+
+                default:
+                    System.out.println("invalid option");
+            }
+
+            System.out.println("result is = " +result);
+
+        }
+    }
+
