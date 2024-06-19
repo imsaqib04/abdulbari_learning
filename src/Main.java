626d89217925b26a6279acc4bb80409c53b8d4fb
Mohd Saqib Ansari <you@example.com>
2024-06-19 22:59:16 +0530

    .

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        StudentTest s1 = new StudentTest();
+        s1.name = "Saqib";
+        s1.course = "Computer Science";
+        s1.m1 = 90;
+        s1.m2 = 65;
+        s1.m3 = 60;
+        s1.roll_no = 7;
+        System.out.print(s1);
+        System.out.println("Total:"+s1.total());
+        System.out.println("Average:" +s1.average());
+        System.out.println("grade:" +s1.grade());
+            }
+        }
+        /*import java.awt.*;
+
+public class RunTime {
+    public static void main(String[] args) {
+
+        /*Math_operations.Circle c1 = new Math_operations.Circle();
+        Math_operations.Circle c2 = new Math_operations.Circle();
+        c1.radius = 5;
+        c2.radius = 10;
+        System.out.println(c1.area());
+        System.out.println(c2.area());
+        System.out.println(c1.circumference());
+        System.out.println(c2.circumference());
+
+        System.out.println("\n");
+
+        Math_operations.Rectangle r1 = new Math_operations.Rectangle();
+        r1.length = 5;
+        r1.breadth = 5;
+        System.out.println(r1.area());
+        System.out.println(r1.perimeter());
+        System.out.println(r1.isSquare());*/
+
+//        Rectangle2 r=new Rectangle2();
+//        r.setLength(10.5);//check with negative values.
+//        r.setBreadth(5.5);
+//
+//        System.out.println("Area "+r.area());
+//        System.out.println("Perimeter "+r.perimeter());
+//        System.out.println("Is Square "+r.isSquare());
+//
+//        System.out.println("Length "+r.getLength());
+//        System.out.println("Breadth "+r.getBreadth());
+//        }*/
+//
